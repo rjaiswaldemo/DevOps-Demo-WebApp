92ce86efc1cd97b1cf9304756a9142513cbd1b5e
Ubuntu <ubuntu@ip-172-31-31-193.us-east-2.compute.internal>
2021-02-08 01:53:24 +0000

    updating DB ips

--- src/test/java/servlet/cancelpage.java
+++ src/test/java/servlet/cancelpage.java
@@ -17,7 +17,7 @@ public class cancelpage extends HttpServlet {
 				try
 				{
 					Class.forName("org.postgresql.Driver");
-					String jdbcUrl="jdbc:postgresql://18.220.11.117:5432/postgres";
+					String jdbcUrl="jdbc:postgresql://18.188.137.233:5432/postgres";
 					String username="postgres";
 					String password="password";
 					
