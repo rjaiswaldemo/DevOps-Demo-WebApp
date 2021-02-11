e559ff440715f9ac7e1d9cc9c1a4c608dece671a
Rakesh Jaiswal <inforakeshjaiswal@gmail.com>
2021-02-11 22:59:48 +0000

    updating DB Url

--- src/test/java/servlet/cancelpage.java
+++ src/test/java/servlet/cancelpage.java
@@ -17,7 +17,7 @@ public class cancelpage extends HttpServlet {
 				try
 				{
 					Class.forName("org.postgresql.Driver");
-					String jdbcUrl="jdbc:postgresql://34.122.113.186:5432/postgres";
+					String jdbcUrl="jdbc:postgresql://34.123.150.127:5432/postgres";
 					String username="postgres";
 					String password="password";
 					
