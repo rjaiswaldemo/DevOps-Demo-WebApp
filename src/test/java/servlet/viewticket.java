9db6461aa176f9a264e46ad5336c508d9610126b
Rakesh Jaiswal <inforakeshjaiswal@gmail.com>
2021-02-12 19:09:36 +0000

    Updating DB url for PROD

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://34.123.150.127:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://34.67.171.111:5432/postgres";
 			String username="postgres";
 			String password="password";
 						
